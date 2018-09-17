8a86d400da994093295b31ff1a64fb6b9e426c8b
威濠 夏 <xiaweihao@outlook.com>
2018-09-17 14:05:57 +0800

    更新007

---
+++ leetcode007/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1534236469;
+
+            Console.WriteLine(Reverse(num));
+
+            Console.ReadKey();
+        }
+
+        public static int Reverse(int x)
+        {
+            long result=0;
+            while (x != 0)
+            {
+                var num = x % 10;
+                if (result > Int32.MaxValue / 10)
+                {
+                    return 0;
+                }
+                else if (result < Int32.MinValue / 10)
+                {
+                    return 0;
+                }
+
+                x = x / 10;
+
+                result = result * 10 + num;
+            }
+
+        
+
+            return (int)result;
+
+        }
+    }
+}
